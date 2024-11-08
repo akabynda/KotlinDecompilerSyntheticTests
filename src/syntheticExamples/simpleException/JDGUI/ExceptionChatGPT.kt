1f899b720e7a3707975c3288614a9902d079bfd3
akabynda <st084331@student.spbu.ru>
2024-11-09 02:19:10 +0300

    JDGUI simpleExceptionChatGPT

--- src/syntheticExamples/simpleException/JDGUI/ExceptionChatGPT.kt
+++ src/syntheticExamples/simpleException/JDGUI/ExceptionChatGPT.kt
@@ -1,1 +1,9 @@
 package syntheticExamples.simpleException.JDGUI
+
+fun main() {
+    try {
+        val i = 10 / 0
+    } catch (e: ArithmeticException) {
+        println("Division by zero!")
+    }
+}
