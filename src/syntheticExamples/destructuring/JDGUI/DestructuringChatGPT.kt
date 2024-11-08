72dd95f08c7c2c878744ceb4443c7e859d4cda1a
akabynda <st084331@student.spbu.ru>
2024-11-09 02:05:24 +0300

    JDGUI DestructuringChatGPT

---
+++ src/syntheticExamples/destructuring/JDGUI/DestructuringChatGPT.kt
@@ -0,0 +1,9 @@
+package syntheticExamples.destructuring.JDGUI
+
+data class Point(val x: Int, val y: Int)
+
+fun main() {
+    val point = Point(10, 20)
+    val (x, y) = point
+    println("x = $x, y = $y")
+}
