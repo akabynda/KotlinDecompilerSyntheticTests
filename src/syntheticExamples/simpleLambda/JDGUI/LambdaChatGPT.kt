7ff24f8815c56ea5592371be5f2e1731bdaf3661
akabynda <st084331@student.spbu.ru>
2024-11-09 02:20:06 +0300

    JDGUI LambdaChatGPT

--- src/syntheticExamples/simpleLambda/JDGUI/LambdaChatGPT.kt
+++ src/syntheticExamples/simpleLambda/JDGUI/LambdaChatGPT.kt
@@ -1,1 +1,12 @@
 package syntheticExamples.simpleLambda.JDGUI
+
+fun main() {
+    val arrayOfInteger = arrayOf(1, 2, 3, 4, 5)
+    val numbers = arrayOfInteger.toList()
+
+    numbers.forEach {
+        val n = it
+        val i = n * n
+        println(i)
+    }
+}
