e175bcdff66a3a778d2363946d8bcc3fb768776b
akabynda <st084331@student.spbu.ru>
2024-11-10 17:32:57 +0300

    CFR OperatorOverloadingChatGPT

---
+++ src/syntheticExamples/operatorOverloading/CFR/OperatorOverloadingChatGPT.kt
@@ -0,0 +1,14 @@
+package syntheticExamples.operatorOverloading.CFR
+
+data class Vector(val x: Int, val y: Int) {
+    operator fun plus(other: Vector): Vector {
+        return Vector(x + other.x, y + other.y)
+    }
+}
+
+fun main() {
+    val v1 = Vector(1, 2)
+    val v2 = Vector(3, 4)
+    val v3 = v1 + v2
+    println(v3)
+}
