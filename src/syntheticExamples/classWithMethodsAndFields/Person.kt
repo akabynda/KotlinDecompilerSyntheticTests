1c6eeddc208d72c9721ecdda3e47e05be89b3f5b
akabynda <st084331@student.spbu.ru>
2025-03-07 23:46:29 +0300

    Person to one file

--- src/syntheticExamples/classWithMethodsAndFields/Person.kt
+++ src/syntheticExamples/classWithMethodsAndFields/Person.kt
@@ -1,7 +1,12 @@
-package syntheticExamples.classWithMethodsAndFields.Person
+package syntheticExamples.classWithMethodsAndFields
 
 class Person(private val name: String, private val age: Int) {
     fun sayHello() {
         println("Привет, меня зовут $name")
     }
+}
+
+fun main() {
+    val person = Person("Alice", 29)
+    person.sayHello()
 }
--- src/syntheticExamples/classWithMethodsAndFields/Person/Main.kt
+++
@@ -1,6 +0,0 @@
-package syntheticExamples.classWithMethodsAndFields.Person
-
-fun main() {
-    val person = Person("Alice", 29)
-    person.sayHello()
-}
